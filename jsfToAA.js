b288c8ac4671d88c8c0139e1a4e86b919a94a7cf
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2022-03-18 23:57:14 +0900

    show progress

--- jsfToAA.js
+++ jsfToAA.js
@@ -7,6 +7,10 @@ let count = 0;
 while (jsf.length) {
     result += count >= logo.length || logo[count] === '*' ? jsf.shift() : logo[count];
     count++;
+    !(count % 1000) && console.log(`count: ${count}, ${(1 - (jsf.length / start)) * 100}%`);
+    count === logo.length && console.log(`rest: ${jsf.length}, count: ${count}`);
 }
+
+count < logo.length && console.log(`rest: ${count - logo.length}, count: ${count}`);
 
 fs.writeFileSync('./build.js', result);
